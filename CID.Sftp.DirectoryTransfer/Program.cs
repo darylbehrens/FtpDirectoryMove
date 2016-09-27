e3fb2df74cc06de513b5497be24032a7f1c9b577
Daryl Behrens <Daryl Behrens>
2016-09-26 19:45:08 -0700

    Initial Commit

---
+++ CID.Sftp.DirectoryTransfer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinSCP;
+using Serilog;
+using System.Configuration;
+
+namespace CID.Sftp.DirectoryTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            string baseDirString = appSettings["BaseDirectory"];
+            string sentDirString = appSettings["SentDirectory"];
+
+            // Setup Options for FTP Session
+            SessionOptions sessionOptions = new SessionOptions()
+            {
+                Protocol = Protocol.Ftp,
+                HostName = appSettings["FtpAddress"],
+                UserName = appSettings["FtpLogin"],
+                Password = appSettings["FtpSecret"]
+            };
+
+            // Setup Transfer Options for FTP Session
+            TransferOptions transferOp = new TransferOptions()
+            {
+                TransferMode = TransferMode.Binary
+            };
+
+            // Link to Base Directory, and Create Array of Strings With Full Directory and Individual Directroy Name
+            DirectoryInfo baseDir = new DirectoryInfo(baseDirString);
+            var subFolders = baseDir.GetDirectories().Select(x => new { x.FullName, x.Name });
+
+            try
+            {
+                using (Session session = new Session())
+                {
+                    // Open Session
+                    session.Open(sessionOptions);
+
+                    // Loop Through Directroy Full names and Individual Names
+                    foreach (var dir in subFolders)
+                    {
+                        // Upload File
+                        TransferOperationResult transferResult;
+                        transferResult = session.PutFiles(dir.FullName + @"\*", appSettings["FtpDirectory"], false, transferOp);
+                        transferResult.Check();
+
+                        // For Logging
+                        foreach (TransferEventArgs error in transferResult.Transfers)
+                        {
+                            Console.WriteLine($"File {error.FileName} has been transfered to FTP site at {error.Destination}");
+                            // Need to Find Out What Information needs to be logged
+                        }
+
+                        // Move Directroy To Sent Directory
+                        string sentDir = Path.Combine(sentDirString, dir.Name);
+                        Directory.Move(dir.FullName, sentDir);
+                    }
+                }
+            }
+
+            catch (IOException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+                // Log IO Exception from Directroy Move
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Log Generic Exception which is what is thrown by WinScp
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ CID.Sftp.DirectoryTransfer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CID.Sftp.DirectoryTransfer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CID.Sftp.DirectoryTransfer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f6a9987-42a6-4abd-af7a-4a9e4e955dfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
